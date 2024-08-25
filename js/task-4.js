abf9c91609adf84b819c943cb7c63e983abb9b61
Elena Zhuvak <elenkazh@gmail.com>
2024-08-25 19:13:58 +0100

    adds 4th task

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const trimmedEmail = email.trim();
+    const password = form.elements.password.value;
+    const trimmedPassword = password.trim();
+
+    if(trimmedEmail === '' || trimmedPassword === '') {
+        return alert('All form fields must be filled in');
+    }
+
+    console.log(`Email: ${trimmedEmail}, Password: ${trimmedPassword}`);
+    form.reset();
+}
