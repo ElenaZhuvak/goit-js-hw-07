02b169e2e27fea1bbe829f1e084902eaefc69a11
Elena Zhuvak <elenkazh@gmail.com>
2024-08-26 16:08:02 +0100

    adds 5th task

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,16 @@
+const btn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const body = document.querySelector('body');
+
+
+
+btn.addEventListener('click', getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const randomColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  span.textContent = randomColor;
+  
+  return body.style.backgroundColor = randomColor;
 }
